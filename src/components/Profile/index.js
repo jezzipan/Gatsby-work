459505c2d378c0c5fb448bad2d5fc87749185050
jezzipan <jessica.leute@gmail.com>
2020-09-04 22:30:49 -0300

    create Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Jessica Oliveira</h1>
+        <h2>
+            Web Developer | Data Scientist 
+        </h2>
+        <p>
+            Software developer. Areas of interest: web developer, UX, accessibility web, mathematical modeling, artificial intelligence, 
+            machine learning, public policy, arts and cyber security.
+            (ela/she/her)
+         </p>
+    </div>
+)
+
+export default Profile
