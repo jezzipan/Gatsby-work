187130e080bf835567eb1b8b56c505a18d6613e7
jezzipan <jessica.leute@gmail.com>
2020-09-10 17:00:02 -0300

    icones na sidebar

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,11 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Medium } from "@styled-icons/boxicons-logos/Medium"
+
+const Icons = {
+  Github,
+  Linkedin,
+  Medium
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+const handle = "jessica"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/jezzipan`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/jezzpan/`,
+  },
+  {
+    label: "Medium",
+    url: `https://www.linkedin.com/in/jezzpan/`,
+  },
+]
+
+export default links
