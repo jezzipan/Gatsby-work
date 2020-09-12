b9b059b457be6a938ac5f96c3db24b20f95aa65f
jezzipan <jessica.leute@gmail.com>
2020-09-11 21:09:03 -0300

    inserindo estilos no layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Sidebar from "../Sidebar"
+import MenuBar from "../MenuBar"
 
-import styled from 'styled-components'
+import * as S from "./styled"
 import GlobalStyles from '../../styles/global'
-
-const LayoutWrapper = styled.section `
-  display: flex;
-`
-const LayoutMain = styled.main `
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`
 
 const Layout = ({ children }) => {
   
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <Sidebar />
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
+    </S.LayoutWrapper>
   )
 }
 
@@ -31,3 +23,9 @@ Layout.propTypes = {
 }
 
 export default Layout
+
+
+
+
+
+
