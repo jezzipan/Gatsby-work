8dbf64a8364354c537653c79fa03ead7cbe758cc
jezzipan <jessica.leute@gmail.com>
2020-09-15 16:59:54 -0300

    criando post com o metodo creatPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,52 @@
+const path = require('path')
 const { createFilePath } = require(`gatsby-source-filesystem`)
 // To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
     // Ensures we are processing only markdown files
     if (node.internal.type === "MarkdownRemark") {
-      // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
-      const slug = createFilePath({
-        node,
-        getNode,
-        basePath: "pages",
-      })
-  
-      // Creates new query'able field with name of 'slug'
-      createNodeField({
-        node,
-        name: "slug",
-        value: `/${slug.slice(12)}`,
-      })
+        // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
+        const slug = createFilePath({
+            node,
+            getNode,
+            basePath: "pages",
+        })
+
+        // Creates new query'able field with name of 'slug'
+        createNodeField({
+            node,
+            name: "slug",
+            value: `/${slug.slice(12)}`,
+        })
     }
-  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    return graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+          }
+        }
+      }
+      `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage(
+
+                {
+                    path: node.fields.slug,
+                    components: path.resolve('./src/templates/blog-post.js'),
+                    context: {
+                        slug: node.fields.slug
+                    }
+                }
+            })
+        })
+    })
+}
